9ee572de4a681452d763ba76564c92ac435602d9
Shrayas <shrayasr@gmail.com>
2017-06-03 11:02:15 +0530

    Implement last update model, had forgotten, hehe

--- pinboard.net/Models/LastUpdate.cs
+++ pinboard.net/Models/LastUpdate.cs
@@ -1,6 +1,11 @@
-﻿namespace pinboard.net.Models
+﻿using Newtonsoft.Json;
+using System;
+
+namespace pinboard.net.Models
 {
     public class LastUpdate
     {
+        [JsonProperty("update_time")]
+        public DateTimeOffset UpdateTime { get; set; }
     }
 }
