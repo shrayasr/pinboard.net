196b2569c8ccbe7bf4074a6bc96432412595717d
Shrayas <shrayasr@gmail.com>
2017-06-04 10:06:25 +0530

    Reuse the TagsConverter across multiple classes
    Thanks to https://stackoverflow.com/questions/44350350/writing-reusable-jsonconverters-for-json-net

--- pinboard.net/Util/Converters/TagsConverter.cs
+++ pinboard.net/Util/Converters/TagsConverter.cs
@@ -2,12 +2,23 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 
 namespace pinboard.net.Util.Converters
 {
-    class TagsConverter : JsonConverter
+    public class TagsConverter : JsonConverter
     {
+        private string _delimiter;
+
+        public TagsConverter()
+        {
+            _delimiter = " ";
+        }
+
+        public TagsConverter(string delimiter)
+        {
+            _delimiter = delimiter;
+        }
+
         public override bool CanConvert(Type objectType)
         {
             return (objectType == typeof(string));
@@ -15,19 +26,22 @@ namespace pinboard.net.Util.Converters
 
         public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
         {
-            return reader
-                    .Value
-                    .ToString()
-                    .Split(' ')
+            var value = reader.Value.ToString().Trim();
+
+            if (value.IsEmpty())
+                return new List<string>();
+
+            return value
+                    .Split(_delimiter.ToCharArray())
                     .ToList();
         }
 
         public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
         {
             var list = (List<string>)value;
-            var commaSepList = string.Join(" ", list);
+            var delimitedList = string.Join(_delimiter, list);
 
-            writer.WriteValue(commaSepList);
+            writer.WriteValue(delimitedList);
         }
     }
 }
